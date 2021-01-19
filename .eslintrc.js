0e915ac5ab6936e844e1f8c42428d53e5b35f734
Alexandre Henrique <zaleortgames@outlook.com>
2021-01-19 19:06:40 +0100

    Reglas linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
@@ -14,5 +14,39 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "max-len": [1, { "code": 120 }],
+    "linebreak-style": "off",
+    "no-return-assign": "off",
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": 2,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": false
+      }
+    }],
+    "no-explicit-any": "off",
+    "no-underscore-dangle": "off",
+    "lines-between-class-members": ["error", "always", { exceptAfterSingleLine: true }],
+    "no-param-reassign": "off",
+    "@typescript-eslint/ban-ts-ignore": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-this-alias": "off",
+    "arrow-parens": "off",
+    "no-restricted-globals": "off",
+    "no-plusplus": "off",
+    "no-nested-ternary": "off",
+    "global-require": "off",
+    "import/no-dynamic-require": "off",
   },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
 };
